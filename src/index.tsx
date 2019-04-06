452a4bf60718080087cdee367b4a3b6a7b78c35a
shivani mishra <sm@patientsky.com>
2019-04-07 01:33:13 +0200

    refactored authentication for new login mech

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from './components/app'
 import Form from './components/Form'
 import SiteDetail from './components/SiteDetail'
+import Login from './components/Login'
+import NavBar from './components/NavBar'
+import { AuthenticatedRoute } from './util/Authentication'
 
 const route = (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/add" component={Form} />
-      <Route path="/update/:id" component={Form} />
-      <Route path="/detail/:id" component={SiteDetail} />
-    </Switch>
-  </Router>
+  <div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <AuthenticatedRoute exact path="/" component={App} />
+        <AuthenticatedRoute exact path="/add" component={Form} />
+        <AuthenticatedRoute exact path="/update/:id" component={Form} />
+        <AuthenticatedRoute exact path="/detail/:id" component={SiteDetail} />
+      </Switch>
+    </Router>
+  </div>
 )
 
 ReactDOM.render(route, document.getElementById('root'))
