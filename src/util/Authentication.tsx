f3ac9a5f0d3f375e96fad769a7512cb6c203f382
shivani mishra <sm@patientsky.com>
2019-04-07 01:29:40 +0200

    adding a class file acting as model for authentication

---
+++ src/util/Authentication.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import * as Cookies from 'js-cookie'
+import axios from 'axios'
+import { Route } from 'react-router-dom'
+import Login from '../components/Login'
+
+export const getAccessToken = () => Cookies.get('access_token')
+export const isAuthenticated = () => !!getAccessToken()
+
+export const authenticate = async (email: string, password: string) => {
+  return await axios
+    .post(`https://api.staging-cobiro.com/api/v1/login`, { email: email, password: password })
+    .then((tokens: any) => {
+      //refresh token can be configured as well
+      Cookies.set('access_token', tokens.data.access_token, { expires: tokens.data.expires_in })
+      return true
+    })
+    .catch(error => {
+      console.error('Login failed', error)
+      return false
+    })
+}
+
+export const removeAuth = () => {
+  Cookies.remove('access_token')
+}
+
+export const AuthenticatedRoute = ({ component: Component, exact, path }) => <Route exact={exact} path={path} render={props => (this.isAuthenticated() ? <Component {...props} /> : <Login />)} />
